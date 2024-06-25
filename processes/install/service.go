91f921e83818030bb073567f36357efbfd0edced
Alexey Havrylov <vildulv@gmail.com>
2024-06-25 18:50:02 +0300

    Update install

--- processes/install/service.go
+++ processes/install/service.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 
 	"gitea.bridge.digital/bridgedigital/db-manager-client-cli-go/services"
 	"gitea.bridge.digital/bridgedigital/db-manager-client-cli-go/services/predefined"
@@ -18,7 +17,7 @@ const DestinationPath = "/usr/local/bin/"
 func Execute() {
 	_, err := os.Stat(DestinationPath + services.AppName)
 	if err == nil {
-		fmt.Println(predefined.BuildOk("The application has been installed successfully"))
+		fmt.Println(predefined.BuildOk("Application has already installed"))
 		return
 	}
 
@@ -28,79 +27,31 @@ func Execute() {
 		return
 	}
 
-	var sourcePath string = configDir + "/bin/" + services.AppName
+	var sourceAppPath string = configDir + "/bin/" + services.AppName
 
-	_, errApp := os.Stat(sourcePath)
+	_, errApp := os.Stat(sourceAppPath)
 	if errApp != nil {
 		fmt.Println(predefined.BuildError("Executable file missing"))
 		return
 	}
 
-	/* if envfile.IsEnvFileExist(true) {
-		fmt.Println(predefined.BuildOk("Application has already installed"))
+	errInst := createAppLink(sourceAppPath)
+	if errInst != nil {
+		fmt.Printf(predefined.BuildError("Error executing command: %v\n"), err)
 		return
-	} else { */
-	//createLink()
-	testw(sourcePath)
-	//}
+	}
+
+	fmt.Println(predefined.BuildOk("The application has been installed successfully"))
 }
 
-func createLink() {
-	configDir, errDir := services.CurrentAppDir()
-	if errDir != nil {
-		fmt.Printf(predefined.BuildError("Cannot get current APP directory: %W.\n"), errDir)
-		return
-	}
-
-	command := fmt.Sprintf("export PATH=\"%s/bin:$PATH\" \n", configDir)
-	bashProfileCandidates := []string{".bashrc", ".bash_profile"}
-	homeDir := os.Getenv("HOME")
-
-	for _, bashProfileCandidate := range bashProfileCandidates {
-		candidateFilePath := filepath.Join(homeDir, bashProfileCandidate)
-
-		if _, err := os.Stat(candidateFilePath); err == nil {
-			file, err := os.OpenFile(candidateFilePath, os.O_APPEND|os.O_WRONLY, 0644)
-
-			if err != nil {
-				fmt.Printf(predefined.BuildError("Error opening file %s: %v"), candidateFilePath, err)
-				continue
-			}
-
-			defer file.Close()
-
-			if _, err := fmt.Fprintln(file, command); err != nil {
-				fmt.Printf(predefined.BuildError("Error writing to file %s: %v"), candidateFilePath, err)
-				return
-			}
-		}
-	}
-}
-
-func testw(sourcePath string) {
-	// Define the source and destination paths
-	homeDir := os.Getenv("HOME")
-	if homeDir == "" {
-		fmt.Println("HOME environment variable is not set.")
-		return
-	}
-	//source := configDir + "/bin/" + services.AppName
-	//destination := "/usr/local/bin/db-manager"
-
-	// Create the command to execute
+func createAppLink(sourcePath string) error {
 	cmd := exec.Command("sudo", "ln", "-s", sourcePath, DestinationPath)
-
-	// Set the environment variables for the command
 	cmd.Env = os.Environ()
 
-	// Run the command and capture any errors
-	output, err := cmd.CombinedOutput()
+	_, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Error executing command: %v\n", err)
-		fmt.Printf("Output: %s\n", string(output))
-		return
+		return err
 	}
 
-	// Print the output of the command
-	//fmt.Printf("Command executed successfully: %s\n", string(output))
+	return nil
 }
